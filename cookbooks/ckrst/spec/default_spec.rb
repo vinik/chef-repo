e196e214db2cd312516eb59db376be273cd32f4d
vinik <vinicius.kirst@gmail.com>
2016-03-29 22:32:23 -0300

    more tests

--- cookbooks/ckrst/spec/default_spec.rb
+++ cookbooks/ckrst/spec/default_spec.rb
@@ -3,9 +3,14 @@ require 'chefspec'
 describe 'ckrst::default' do
   let (:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
-  it 'should create docker service' do
+  it 'creates docker service' do
     chef_run.converge(described_recipe)
     expect(chef_run).to create_docker_service("default")
+  end
+
+  it 'pulls ubuntu image' do
+    chef_run.converge(described_recipe)
+    expect(chef_run).to pull_docker_image("ubuntu")
   end
 
 
